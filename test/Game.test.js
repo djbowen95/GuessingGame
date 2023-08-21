2550a89f3777c1f6802d706c4fd35060164544f1
Daniel Bowen <djbowen95@gmail.com>
2023-08-21 21:57:14 +0100

    Outline framework of tests for first two parts of game

--- test/Game.test.js
+++ test/Game.test.js
@@ -7,11 +7,28 @@ describe('Initialisation', () => {
         expect(game.guessesLeft).toBeDefined();
         expect(typeof (game.guessesLeft)).toBe('number');
     });
-    
-    it('make guess method', () => {});
+
+
     it('play method', () => {});
-    it('quit method', () => {});
-    it('play again method', () => {});
-    it('ask for letter method', () => {});
     it('next word method', () => {});
-})
+    
+    it('ask for letter method', () => {});
+    it('make guess method', () => {});
+
+    it('quit method', () => {});
+    it('ask to play again method', () => {});    
+   
+});
+
+describe('Start a new game with .play()', () => {
+    it('sets the guessesLeft to 10', () => {});
+    it('runs function to select a word', () => {});
+});
+
+describe('Select a new word with .nextWord()', () => {
+    it('selects a word from the words module', () => {});
+    it('the word from the list is random', () => {});
+    it('creates a new instance of the Word class', () => {});
+    it('logs the starting string to the console', () => {});
+    it('runs the command to prompt the user for input', () => {});
+});
